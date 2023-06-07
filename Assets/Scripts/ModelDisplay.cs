ffc30cf333fb08583de6b34aa7990220f14c373b
Julien Constant <julienconstant190@gmail.com>
2023-06-07 16:24:50 +0200

    Update .obj import function to import them from `Assets/Resources/Bricks`

--- Assets/Scripts/ModelDisplay.cs
+++ Assets/Scripts/ModelDisplay.cs
@@ -12,7 +12,6 @@ public class ModelDisplay : MonoBehaviour
     public Camera camTest;
 
     public GameObject par;
-
     public GameObject piece;
 
     public float scaling;
@@ -33,22 +32,30 @@ public class ModelDisplay : MonoBehaviour
         this.Display("3004", new Vector3(0, 0, 100), new Quaternion(0.7f, 0f, 0f, 0.7f), "#fff101");
     }
 
-    void Display(string nomPiece, Vector3 pos, Quaternion rot, string hexColor)
+    /// <summary>
+    /// Affiche la piece donnee sur la scene
+    /// </summary>
+    /// <param name="name">Nom de la piece a importer</param>
+    /// <param name="pos">Position de la piece dans la scene</param>
+    /// <param name="rot">Rotation de la piece dans la scene</param>
+    /// <param name="hexColor">Couleur de la piece</param>
+    void Display(string name, Vector3 pos, Quaternion rot, string hexColor)
     {
-        piece = new OBJLoader().Load("C:/Users/gauth/Documents/GitHub/API/" + nomPiece + ".obj");
         // Loading new piece
-        // Positioning the piece
+        piece = new OBJLoader().Load(@$"Assets/Resources/Bricks/{name}.obj");
         piece.transform.position = pos;
-        // Rotating the piece
         piece.transform.rotation = rot;
+        
         // Scaling the piece
         piece.transform.localScale = new Vector3(scaling, scaling, scaling);
+        
         // Setting the AR controller as a parent
         piece.transform.parent = par.transform;
+        
         // Changing the color
-        Color customColor;
-        if (ColorUtility.TryParseHtmlString(hexColor, out customColor))
+        if (ColorUtility.TryParseHtmlString(hexColor, out Color customColor)) 
             piece.GetComponentInChildren<Renderer>().material.color = customColor;
+
         UnityEngine.Debug.Log(customColor);
     }
 }
